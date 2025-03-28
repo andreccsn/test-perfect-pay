23c66cfdc17b8e46b8c7b5899c8f89be6128bbe6
André Cândido <candidocarvalho.andre@gmail.com>
2025-03-28 10:57:05 -0300

    Defining Transaction and Customer models

---
+++ app/Models/Transaction.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    /**
+     * Customer relation belongsTo
+     * @return BelongsTo
+     */
+    public function customer(): BelongsTo
+    {
+        return $this->belongsTo(Customer::class);
+    }
+}
